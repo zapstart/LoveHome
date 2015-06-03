334d04913343c1c75a73df601b1f263bff1c76ee
zapstart <zapstart@gmail.com>
2015-06-03 13:39:04 -0700

    add edit page

--- mysite/lovehome/urls.py
+++ mysite/lovehome/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from .import views 
 
 urlpatterns = [
-    url(r'^front/$',                 views.show_front, name = 'front_page'),
-    url(r'^edit/$',                  views.add_blog,   name = 'edit_blog'),
-    url(r'^page/(?P<page_id>\w+)/$', views.show_blog,  name = 'blog_page'),
+    url(r'^$',                           views.show_front, name = 'front_page'),
+    url(r'^add/$',                       views.add_blog,   name = 'add_blog'),
+    url(r'^edit/(?P<page_id>\w+)/$',     views.edit_blog,  name = 'edit_blog'),
+    url(r'^page/(?P<page_id>\w+)/$',     views.show_blog,  name = 'blog_page'),
 ]
