1d1719d0e73e323f0509c6b67d70c70d12da1975
zapstart <zapstart@gmail.com>
2015-06-15 14:06:32 -0700

    add blog panel; change css style for front page

--- mysite/lovehome/models.py
+++ mysite/lovehome/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class blog_text(models.Model):
     title          = models.CharField(max_length = 50) 
-    b_created_time = models.TimeField(auto_now_add = True, blank = True)
-    b_update_time  = models.TimeField(auto_now = True, blank = True)
+    b_created_time = models.DateTimeField(auto_now_add = True, blank = True)
+    b_update_time  = models.DateTimeField(auto_now = True, blank = True)
     blog_content   = models.TextField(blank = True)
     
